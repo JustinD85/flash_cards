4805e7a27e1d707124dc4ce0ece23a98a2e44b2c
Justin <justin@baal-software.com>
2019-03-11 21:18:24 -0600

    Add Turn class and Test

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+class Turn
+  attr_reader :guess, :card
+
+  def initialize(g,c)
+    @guess = g
+    @card = c
+  end
+
+  def correct?
+    @guess == @card.answer
+  end
+
+  def feedback
+    self.correct? ? "Correct!" : "Incorrect."
+  end
+end
---
+++ test/turn_test.rb
@@ -0,0 +1,47 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/turn'
+require './lib/card'
+
+class TurnTest < MiniTest::Test
+
+  def setup
+    @card = Card.new("What is the capitol of Massachusetts?", "Boston",:Geography)
+    @turn = Turn.new("Boston", @card)
+  end
+
+  def test_it_exists
+    assert_instance_of Turn, @turn
+  end
+
+  def test_it_returns_the_guess
+    assert_equal "Boston", @turn.guess
+  end
+
+  def test_it_should_return_card
+    assert_equal @card, @turn.card
+  end
+
+  def test_it_returns_true
+    assert_equal true, @turn.correct?
+  end
+
+  def test_it_returns_false_with_wrong_guess
+    turn = Turn.new("Miami", @card)
+    assert_equal false, turn.correct?
+  end
+
+  def test_it_returns_false_when_given_wrong_args
+    turn = Turn.new(1,@card)
+    assert_equal false, turn.correct?
+  end
+
+  def test_it_should_return_correct
+    assert_equal "Correct!", @turn.feedback
+  end
+
+  def test_it_should_return_incorrect_with_wrong_guess
+    turn = Turn.new("Miami", @card)
+    assert_equal "Incorrect.", turn.feedback
+  end
+end
