51badcc1460463923de127097910dac8687f1fec
Justin <justin@baal-software.com>
2019-03-17 17:51:04 -0600

    Complete Iteration four

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -7,4 +7,15 @@ class CardGenerator
   @filename = filename
   end
 
+  def cards
+    file = File.open(filename)
+    prompts = file.read.split("\n")
+    prompts.map do |prompt|
+      card_info = prompt.split(",")
+      question = card_info.shift
+      answer = card_info.shift
+      category = card_info.shift.to_sym
+      Card.new(question,answer,category)
+    end
+  end
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,16 +1,29 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card_generator'
+require './lib/card'
 
 class CardGeneratorTest < MiniTest::Test
   attr_reader :card_generator
 
   def setup
-    @card_generator = CardGenerator.new('../lib/cards.txt')
+    @card_generator = CardGenerator.new("./lib/cards.txt")
   end
   
   def test_it_should_exist
     assert_instance_of CardGenerator, card_generator
+  end
+
+  def test_it_should_have_correct_file_path
+    assert_equal "./lib/cards.txt", card_generator.filename
+  end
+  
+  def test_it_should_create_array_of_cards
+    assert_instance_of Array, card_generator.cards
+  end
+
+  def test_it_should_have_only_cards_in_array
+    card_generator.cards.each{ |card| assert_instance_of Card, card }
   end
 
 end
